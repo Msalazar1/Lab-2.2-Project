8a2b54bde6e0bd3d481d867166cfb3e59179482c
Manny <mannysalazar@quickenloans.com>
2020-07-07 16:50:12 -0400

    Lab 2.2 Project

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace TableofPowers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool keepgoing = true;
+            while (keepgoing)
+            {
+                {
+                Console.WriteLine("Learn your squares and cubes!");
+                Console.WriteLine("Enter an integer:");
+
+                string entry = Console.ReadLine();
+                double i = double.Parse(entry);
+                if (i <= 0)
+                {
+                    Console.WriteLine("Please enter a number greater than 0.");
+                }
+
+                double squared = i * i;
+                double cubed = i * i * i;
+                Console.WriteLine("Number\t\t\tSquared\t\t\tCubed");
+                Console.WriteLine("======\t\t\t=======\t\t\t=====");
+                Console.WriteLine($"{entry}\t\t\t{squared}\t\t\t{cubed}");
+
+               
+                    Console.WriteLine("Try again? yes/no");
+                    string go = Console.ReadLine();
+
+                    if (go == "no")
+                    {
+                        keepgoing = false;
+                    }
+
+                    else if (go != "yes")
+                    {
+                        Console.WriteLine("That is not the correct input. You must have meant no.");
+                        keepgoing = false;
+                    }
+
+                }
+                
+            }
+            Console.WriteLine("All set!");
+        }
+    }
+}
+  
+             
